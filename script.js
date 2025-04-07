4839fc8ef795ecdfe5aa14ade44858a975d526a0
Kate Schumacher <kate.schumacher@gmail.com>
2025-04-07 13:55:23 -0500

    Write calculator function with tests

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ function reverseString(string) {
   return String(string).split("").reverse().join("");
 }
 
-export { capitalize, reverseString }
+const calculator = {
+  add: function(x, y) {
+    return Number(x) + Number(y);
+  },
+  subtract: function(x, y) {
+    return Number(x) - Number(y);
+  },
+  multiply: function(x, y) {
+    return Number(x) * Number(y);
+  },
+  divide: function(x, y) {
+    if (Number(y) === 0) {
+      throw new Error('Cannot divide by zero!');
+    }
+    return Number(x) / Number(y);
+  }
+}
+
+export { capitalize, reverseString, calculator }
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./script";
+import { capitalize, reverseString, calculator } from "./script";
 
 test('capitalize function exists', () => {
   expect(capitalize).toBeDefined();
@@ -35,4 +35,43 @@ test('reverseString returns a string, reversed', () => {
   expect(reverseString(12345)).toBe("54321");
   expect(reverseString("hello")).toBe("olleh");
   expect(reverseString("hi there!")).toBe("!ereht ih");
+})
+
+test('calculator object exists', () => {
+  expect(calculator).toBeDefined();
+})
+
+test('calculator has basic operation methods', () => {
+  expect(calculator.add).toBeDefined();
+  expect(calculator.subtract).toBeDefined();
+  expect(calculator.multiply).toBeDefined();
+  expect(calculator.divide).toBeDefined();
+})
+
+test('calculator add method adds numbers correctly', () => {
+  expect(calculator.add(2, 5)).toBe(7);
+  expect(calculator.add(72, 61)).toBe(133);
+  expect(calculator.add(-151, 151)).toBe(0);
+})
+
+test('calculator subtract method subtracts numbers correctly', () => {
+  expect(calculator.subtract(7, 3)).toBe(4);
+  expect(calculator.subtract(-200, -50)).toBe(-150);
+  expect(calculator.subtract(10, 30)).toBe(-20);
+})
+
+test('calculator multiply method multiplies numbers correctly', () => {
+  expect(calculator.multiply(6, 6)).toBe(36);
+  expect(calculator.multiply(-4, 5)).toBe(-20);
+  expect(calculator.multiply(-11, -13)).toBe(143);
+})
+
+test('calculator divide by zero returns error', () => {
+  expect(() => calculator.divide(4, 0)).toThrow();
+})
+
+test('calculator divide method divides numbers correctly', () => {
+  expect(calculator.divide(4, 1)).toBe(4);
+  expect(calculator.divide(10, 5)).toBe(2);
+  expect(calculator.divide(7, 3)).toBe(2.3333333333333335);
 })
